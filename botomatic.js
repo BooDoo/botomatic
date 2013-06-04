9a97144c1f4e1849499c5b4bc88496a73ed6c9cc
Joel McCoy <joelmccoy@gmail.com>
2013-06-04 00:33:28 -0400

    Basic status query via GET at /status

--- botomatic.js
+++ botomatic.js
@@ -11,9 +11,29 @@ var CONFIG      = require('./config.js'),
 
 // This is present for deployment to nodejitsu, which requires some response to http call.
 app.get('/', function(req, res){
-    //res.send('IGNORE ME.');
-    res.send(JSON.stringify(_.keys(Bot.bots),null,2));
+    res.send('IGNORE ME.<br /><br /><a href="http://github.com/BooDoo/botomatic/tree/gcatpix">I am botomatic</a>');
+    //res.send(JSON.stringify(_.keys(Bot.bots),null,2));
 });
+
+//TODO: Render each Bot.bots handle/bot.key as clickable link!
+app.get('/status', function(req, res) {
+    //res.send(JSON.stringify(req.query,null,2));
+    var handle = req.query.handle || "undefined",
+        key = req.query.key || "undefined";
+
+    if (key !== "undefined" && handle !== "undefined") {
+      console.log("Fetching info for Bot.bots[" + handle + "]." + key);
+      res.send(JSON.stringify(Bot.bots[handle][key],null,2));
+    }
+    else if (handle !== "undefined") {
+      console.log("Fetching info for Bot.bots[" + handle + "]");
+      res.send(JSON.stringify(_.keys(Bot.bots[handle]),null,2));
+    } else {
+      console.log("Fetching list of Bot.bots handles");
+      res.send(JSON.stringify(_.keys(Bot.bots),null,2));
+    }
+});
+
 app.listen(process.env.PORT || 3000);
 
 //Immediate function to construct bots and make setInterval calls:
