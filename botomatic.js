150edc5929f92785cd169ac8bcfb7a7f529a497c
Joel McCoy <joelmccoy@gmail.com>
2013-06-04 15:12:07 -0400

    Index of clickable bots at /status

--- botomatic.js
+++ botomatic.js
@@ -15,23 +15,31 @@ app.get('/', function(req, res){
     //res.send(JSON.stringify(_.keys(Bot.bots),null,2));
 });
 
-//TODO: Render each Bot.bots handle/bot.key as clickable link!
 app.get('/status', function(req, res) {
-    //res.send(JSON.stringify(req.query,null,2));
     var handle = req.query.handle || "undefined",
-        key = req.query.key || "undefined";
+        key = req.query.key || "undefined",
+        handles, keys,
+        output = "";
 
     if (key !== "undefined" && handle !== "undefined") {
       console.log("Fetching info for Bot.bots[" + handle + "]." + key);
-      res.send(JSON.stringify(Bot.bots[handle][key],null,2));
+      output = '<pre>' + JSON.stringify(Bot.bots[handle][key],null,'\t') + '</pre>';
     }
     else if (handle !== "undefined") {
       console.log("Fetching info for Bot.bots[" + handle + "]");
-      res.send(JSON.stringify(_.keys(Bot.bots[handle]),null,2));
+      keys = _.keys(Bot.bots[handle]);
+      _.each(keys, function (k, i, o) {
+        output += '<a href="/status?handle=' + handle + '&key=' + k + '">' + k + '</a><br />';
+      });
     } else {
       console.log("Fetching list of Bot.bots handles");
-      res.send(JSON.stringify(_.keys(Bot.bots),null,2));
+      handles = _.keys(Bot.bots);
+      _.each(handles, function (k, i, o) {
+        output += '<a href="/status?handle=' + k + '">' + k + '</a><br />';
+      });
     }
+
+    res.send(output)
 });
 
 app.listen(process.env.PORT || 3000);
