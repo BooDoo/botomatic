fb2e17efe18407b486e7e135d808c717957fa2b3
Joel McCoy <joelmccoy@gmail.com>
2013-06-10 01:16:44 -0400

    Target value is contenteditable, button to replace with new value (insecure)

--- www/public/javascripts/handlers.js
+++ www/public/javascripts/handlers.js
@@ -10,7 +10,7 @@ function clickHandler(e) {
 
   if (currentPrimary)
     currentPrimary.classList.remove('pure-button-primary');
-  
+
   target.classList.add('pure-button-primary');
   postReq.open("POST", endpoint, true);
   postReq.onreadystatechange = function() {
@@ -20,5 +20,50 @@ function clickHandler(e) {
       clearTarget.innerHTML = "";
   }
   postReq.send({"endpoint": endpoint});
-  e.preventDefault(); 
+  e.preventDefault();
+}
+
+function updateClickHandler(e) {
+  var target = e.target,
+      endpoint = target.href,
+      postReq = new XMLHttpRequest(),
+      payload, content,
+      dataTarget = document.getElementById('property-value');
+
+  e.preventDefault();
+  target.classList.add('pure-button-disabled');
+
+  try {
+    content = JSON.parse(dataTarget.innerText);
+  }
+  catch(e) {
+    content = dataTarget.innerText
+  }
+  //console.log(content);
+
+  payload = { "secret": "supersecret",
+              "changes": [
+                {
+                  "action": "replaceValue",
+                  "content": content
+                }
+              ]
+            };
+  payload = JSON.stringify(payload);
+
+  postReq.open("POST", endpoint, true);
+  postReq.setRequestHeader("Content-Type", "application/json");
+  postReq.overrideMimeType("text/plain");
+  postReq.onreadystatechange = function() {
+    if (postReq.readyState !== 4) {
+      if (postReq.status === 200 || postReq.status === 207) {
+        target.innerText = "Success"
+      }
+      else if (postReq.status === 500) {
+        target.innerText = "Failure";
+      }
+    return postReq.status;
+    }
+  };
+  postReq.send(payload);
 }
