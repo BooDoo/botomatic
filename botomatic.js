818f76a3a95bab0b7418e426901ec58b648766dc
BooDoo <joelmccoy@gmail.com>
2013-12-19 19:40:55 -0500

    Actually run server

--- botomatic.js
+++ botomatic.js
@@ -161,6 +161,10 @@ app.get('/store/bots.json',
   }
 );
 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
 //Immediate function to construct bots and make setInterval calls:
 (function (botConfigs) {
   var botHandle = '',
